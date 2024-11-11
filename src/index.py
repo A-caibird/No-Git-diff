534fe7cfe21da344dadd587b82e051e754c51934
Acaibird <durbannhawkey42@gmail.com>
2024-11-11 11:37:19 +0800

    feat: the outputs contains paths of two file
    resolve #1

--- src/index.py
+++ src/index.py
@@ -82,12 +82,13 @@ def main():
     Main function to handle command-line arguments and perform diff.
     """
     if len(sys.argv) != 3:
-        print("Usage: python diff_tool.py <file1> <file2>")
+        print("Usage: dt <file1> <file2>")
         sys.exit(1)
 
     file1_path = sys.argv[1]
     file2_path = sys.argv[2]
-
+    print(file1_path)
+    print(file2_path)
     file1_content = read_file(file1_path)
     file2_content = read_file(file2_path)
 
